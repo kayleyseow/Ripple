ad7da5a21d56c006541858997331482a9d4b2873
Kayley Seow <kayleyseow8@gmail.com>
2021-06-30 18:05:06 -0700

    Finished smoothing out segues, text storage for both affirmations and gratitude are working.

---
+++ Ripple/GratitudeInformationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GratitudeInformationViewController.swift
+//  Ripple
+//
+//  Created by  Scholar on 6/30/21.
+//
+
+import UIKit
+
+class GratitudeInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func goBackButtonSubmit(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
