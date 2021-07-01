0589ef09a272a3952ae7752ac65580614eb791c9
Ellie Austin <elliemac2004@icloud.com>
2021-07-01 10:44:19 -0700

    Edits

---
+++ Ripple/HomeScreenViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeScreenViewController.swift
+//  Ripple
+//
+//  Created by Ellie Austin on 7/1/21.
+//
+
+import UIKit
+
+class HomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
