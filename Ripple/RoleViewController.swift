f3b44f0aeef13b3ea2bb5939fda1cf611259c241
America Luna Herrera <luna.america.we@gmail.com>
2021-07-01 11:21:55 -0700

    alert

---
+++ Ripple/RoleViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RoleViewController.swift
+//  Ripple
+//
+//  Created by  Scholar on 7/1/21.
+//
+
+import UIKit
+
+class RoleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func LightBulbTip(_ sender: Any) {
+        let alertController = UIAlertController(title: "Tip", message: "Today’s Tip", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title:"Got it!", style: UIAlertAction.Style.default, handler: nil ))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
